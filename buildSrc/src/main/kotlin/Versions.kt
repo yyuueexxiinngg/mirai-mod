b0c49c4295f30b7448968e887a98d53122cab4f9
yyuueexxiinngg <yyuueexxiinngg@outlook.com>
2020-08-08 14:04:35 +0800

    Downgrading #1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -14,11 +14,11 @@ object Versions {
 
     object Kotlin {
         const val compiler = "1.4.0-rc"
-        const val stdlib = "1.4.0-rc"
-        const val coroutines = "1.3.8-1.4.0-rc"
-        const val atomicFU = "0.14.3-1.4.0-rc"
-        const val serialization = "1.0-M1-1.4.0-rc"
-        const val ktor = "1.3.2-1.4.0-rc"
+        const val stdlib = "1.3.72"
+        const val coroutines = "1.3.7"
+        const val atomicFU = "0.14.2"
+        const val serialization = "0.20.0"
+        const val ktor = "1.3.2"
         const val binaryValidator = "0.2.3"
 
         const val io = "0.1.16"
